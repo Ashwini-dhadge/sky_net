9a065c8f2d9d6f821f2d0d70b0a53d35c88b8f68
Ashwini-dhadge <ashwini@windhans.com>
2026-04-13 13:24:02 +0530

    updated certificate issued

--- application/views/app/final_certificate_bkp.php
+++ application/views/app/final_certificate_bkp.php
@@ -3,210 +3,88 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>RHCSA Certificate</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <style>
-    body {
-        background: #f2f2f2;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
+        @page {
+            size: 297mm 210mm;
+            margin: 10mm 10mm 10mm 10mm;
+        }
 
-    .certificate {
-        width: 297mm;
-        height: 200mm;
-        background: white;
-        margin: auto;
-        padding: 10mm 25mm;
-        position: relative;
-    }
-
-    .cert-top {
-        font-size: 28px;
-        font-weight: 700;
-    }
-
-    /* NAME */
-    .cert-name {
-        font-size: 62px;
-        color: #e33b2f;
-        font-weight: 700;
-        margin-top: 25px;
-    }
-
-    /* DESCRIPTION */
-    .cert-desc {
-        font-size: 24px;
-        margin-top: 40px;
-    }
-
-    /* TITLE */
-    .cert-title {
-        font-size: 50px;
-        color: #e33b2f;
-        font-weight: 700;
-        margin-top: 30px;
-        line-height: 1.2;
-    }
-
-    /* FOOTER */
-    .cert-footer {
-        position: absolute;
-        bottom: 40mm;
-        left: 25mm;
-        display: flex;
-        align-items: center;
-        gap: 25px;
-    }
-
-    .qr-section {
-        border-right: 1px solid gray;
-        padding-right: 20px;
-    }
-
-    .qr-section img {
-        width: 100px;
-    }
-
-    .cert-details {
-        line-height: 28px;
-        font-size: smaller;
-    }
-
-    /* LOGO RIGHT */
-    .cert-logo {
-        position: absolute;
-        bottom: 40mm;
-        right: 25mm;
-    }
-
-    .cert-logo img {
-        width: 260px;
-    }
-
-    /* BADGE */
-    .cert-badge {
-        padding: 10px;
-        background: #000;
-        position: absolute;
-        top: 60px;
-        right: 70px;
-        border-radius: 5px;
-    }
-
-    .cert-bottom_text {
-        font-weight: 700;
-        position: absolute;
-        bottom: 100px;
-        left: 100px;
-        display: flex;
-        align-items: center;
-        gap: 25px;
-        font-size: 11px;
-    }
-
-    @media print {
-
-        html,
         body {
-            width: 297mm;
-            height: 200mm;
+            font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
             background: white;
         }
-
-        .cert-badge {
-            background: #000 !important;
-            -webkit-print-color-adjust: exact;
-            print-color-adjust: exact;
-        }
-
-        .certificate {
-            width: 297mm;
-            height: 200mm;
-            margin: 0;
-            padding: 20mm 25mm;
-            page-break-after: avoid;
-        }
-
-        @page {
-            size: A4 landscape;
-            margin: 0;
-        }
-
-    }
     </style>
-
 </head>
 
 <body>
 
-    <div class="certificate">
+    <!-- BADGE: top-right black box (absolute, page-relative) -->
+    <div style="position: absolute; top: 10mm; right: 10mm; background: #000000; padding: 10px; width: 55mm;">
+        <table width="100%" cellpadding="0" cellspacing="0" border="0">
+            <tr>
+                <td>
+                    <img src="<?= FCPATH ?>assets/certificate_image/rh_white_logo.png" style="height: 25px;">
+                </td>
+                <td align="right">
+                    <img src="<?= FCPATH ?>assets/certificate_image/certificate_icon.png" style="height: 25px;">
+                </td>
+            </tr>
+        </table>
+        <div style="margin-top: 35px;">
+            <p style="color: #ffffff; font-size: 15px; font-weight: bold; margin: 0; padding: 0;"><?= htmlspecialchars($course_name) ?></p>
+            <p style="color: #ffffff; font-size: 14px; margin: 0; padding: 4px 0 0 0;">System Administrator</p>
+        </div>
+    </div>
 
-        <div class="cert-top">
+    <!-- MAIN CONTENT: flows normally with right padding to avoid badge overlap -->
+    <div style="padding: 5mm 70mm 0 5mm;">
+
+        <p style="font-size: 22px; font-weight: bold; margin: 0; padding-top: 5mm;">
             Red Hat, Inc. hereby certifies that
-        </div>
+        </p>
 
-        <div class="cert-name">
-            <?= $name ?>
-        </div>
+        <p style="font-size: 52px; color: #e33b2f; font-weight: bold; margin: 6mm 0 0 0; line-height: 1.1;">
+            <?= htmlspecialchars($name) ?>
+        </p>
 
-        <div class="cert-desc">
+        <p style="font-size: 20px; margin: 8mm 0 0 0;">
             has successfully completed all program requirements and is certified as a
-        </div>
+        </p>
 
-        <div class="cert-title">
-            Red Hat Certified System<br>
-            Administrator (RHCSA)
-        </div>
+        <p style="font-size: 42px; color: #e33b2f; font-weight: bold; margin: 6mm 0 0 0; line-height: 1.2;">
+            Red Hat Certified System<br>Administrator (RHCSA)
+        </p>
 
+    </div>
 
+    <!-- FOOTER: QR code + details (absolute bottom-left) -->
+    <div style="position: absolute; bottom: 22mm; left: 10mm;">
+        <table cellpadding="0" cellspacing="0" border="0">
+            <tr>
+                <td style="border-right: 1px solid #888888; padding-right: 14px; vertical-align: middle;">
+                    <img src="<?= FCPATH ?>assets/certificate_image/dummy_qr.png" style="width: 80px; height: 80px;">
+                </td>
+                <td style="padding-left: 14px; font-size: 11px; line-height: 22px; vertical-align: middle;">
+                    <div><?= htmlspecialchars($issue_date) ?></div>
+                    <div>Issued by: Red Hat</div>
+                    <div>Verify: <?= htmlspecialchars($verify_url) ?></div>
+                    <div>Certification ID: <?= htmlspecialchars($certification_id) ?></div>
+                </td>
+            </tr>
+        </table>
+    </div>
 
-        <div class="cert-badge">
-            <div style="display: flex; align-items: center; gap: 10px; ">
-                <img src="<?= FCPATH ?>assets/certificate_image/rh_white_logo.png"
-                    style="height: 30px; margin-right: 20px;">
-                <div style="color: white;">
-                    <img src="<?= FCPATH ?>assets/certificate_image/certificate_icon.png" style="height: 30px;">
-                </div>
-            </div>
-            <div style="margin-top: 40px;">
-                <p style="color: white; font-size: 17px; font-weight: 700; margin: 0;padding: 0;"><?= $course_name ?>
-                </p>
-                <p style="color: white; font-size: 17px; margin: 0;padding: 0;">System Administrator</p>
-            </div>
+    <!-- RED HAT LOGO: absolute bottom-right -->
+    <div style="position: absolute; bottom: 22mm; right: 10mm;">
+        <img src="<?= FCPATH ?>assets/certificate_image/rh_black_logo.png" style="width: 200px;">
+    </div>
 
-        </div>
-
-
-
-        <div class="cert-footer">
-
-            <div class="qr-section">
-                <img src="<?= FCPATH ?>assets/certificate_image/dummy_qr.png" alt="">
-            </div>
-
-            <div class="cert-details">
-                <div>December 08, 2025</div>
-                <div>Issued by: Red Hat</div>
-                <div>Verify: https://www.credly.com/badges/d898ed2a-8d6a-49de-8600-bce8d3fb41d8</div>
-                <div>Certification ID: 190-236-588</div>
-            </div>
-
-        </div>
-
-
-
-        <div class="cert-logo">
-            <img src="<?= FCPATH ?>assets/certificate_image/rh_black_logo.png">
-        </div>
-        <div class="cert-bottom_text">
-            <span>Copyright (c) 2022 Red Hat, Inc. All rights reserved. Red Hat is a trademark of Red Hat, Inc.</span>
-        </div>
+    <!-- COPYRIGHT: absolute bottom -->
+    <div style="position: absolute; bottom: 8mm; left: 10mm; font-size: 9px; font-weight: bold;">
+        Copyright (c) 2022 Red Hat, Inc. All rights reserved. Red Hat is a trademark of Red Hat, Inc.
     </div>
 
 </body>
